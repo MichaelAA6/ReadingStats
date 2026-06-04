a1dfbb29e92a4a20281bb5f06b3fe8fa47021530
ImaHoovy <mikey.mojo@icloud.com>
2026-06-04 11:21:11 +0100

    feat: added graph showing the offsides of the forwards displaying the average of the players

--- app/routes.py
+++ app/routes.py
@@ -32,5 +32,5 @@ def midfielders():
 
 @main.route('/forwards')
 def forwards():
-    apps_graph, ga_graph,card_graph,shots_graph = forwards_graph()
-    return render_template('forwards.html',apps_graph=apps_graph,ga_graph=ga_graph,card_graph=card_graph,shots_graph=shots_graph)
+    apps_graph, ga_graph,card_graph,shots_graph,offside_graph = forwards_graph()
+    return render_template('forwards.html',apps_graph=apps_graph,ga_graph=ga_graph,card_graph=card_graph,shots_graph=shots_graph,offside_graph=offside_graph)
